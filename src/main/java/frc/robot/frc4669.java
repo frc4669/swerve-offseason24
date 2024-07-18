714926075b4abdb8625ef4770b84a3ab37649604
Alvin-He <shinypeach36@gmail.com>
2024-07-18 12:08:16 -0700

    more utility functions and documentation

--- src/main/java/frc/robot/frc4669.java
+++ src/main/java/frc/robot/frc4669.java
@@ -3,7 +3,17 @@ package frc.robot;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class frc4669 {
+
+    /**
+     * Applys a joystick lower bound dead zone to an input
+     * 
+     * @param input input value 
+     * @param deadzone lower deadzone
+     * @return double input after deadzone is applied
+     */
     public static double ApplyJoystickDeadZone(double input, double deadzone) {
         if (Math.abs(input) > deadzone) {
             // linear dead zone 
@@ -12,6 +22,13 @@ public class frc4669 {
         return 0; 
     }
 
+    /**
+     * Gets default Falcon500 Configuration
+     * 
+     * @usuage To apply configuration call `motor.getConfigurator().apply(configuration);`
+     * 
+     * @return TalonFXConfiguration 
+     */
     public static TalonFXConfiguration GetFalcon500DefaultConfig() {
         TalonFXConfiguration motorConfig = new TalonFXConfiguration();
         motorConfig.CurrentLimits.SupplyCurrentLimitEnable = true; 
@@ -22,5 +39,20 @@ public class frc4669 {
         motorConfig.MotorOutput.NeutralMode = com.ctre.phoenix6.signals.NeutralModeValue.Coast; 
         motorConfig.MotorOutput.Inverted = com.ctre.phoenix6.signals.InvertedValue.CounterClockwise_Positive; 
         return motorConfig; 
-    }   
+    }
+
+    /**
+     * Check if we are on the Red Alliance
+     * <p>
+     * This method can be used to determine when to apply transformations to paths, cmds, etc.
+     * 
+     * @return `True` if we are on the red alliance, `False` on blue
+     */
+    public static Boolean IsOnRedAlliance() {
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+    }
 }
